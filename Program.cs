af52aa02e1737e84582f25f31428dd48f0779e29
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:31:22 +0700

    menambahkan class pemroses data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, W orld!");
+﻿class PemrosesData
+{
+    public double DapatkanNilaiTerbesar<T>(T data, T data1, T data2)
+    {
+
+        dynamic a = (dynamic)data1;
+        dynamic b = (dynamic)data2;
+        dynamic c = (dynamic)data;
+        dynamic max = a;
+        if (max < b)
+        {
+            max = b;
+        }
+        if (max < c)
+        {
+            max = c;
+        }
+        return max;
+    }
+
+}
+class program
+{
+    static void Main()
+    {
+        double a = 1.0;
+        double b = 3.0;
+        double c = 2.2;
+        PemrosesData data = new PemrosesData();
+        Console.WriteLine("aaaaaaaa");
+    }
+}
